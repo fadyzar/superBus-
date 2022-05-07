1fab941b0556688634620e6b0d0a6fe4a282249f
fadyzar <fadyzarka978@gmail.com>
2022-05-08 01:56:46 +0300

    superBus

--- mainBus.c
+++ mainBus.c
@@ -1,7 +1,4 @@
-//
-//  main.c
-//  proj2debug
-//
+
 //  Created by fady zarka on 23/04/2022.
 //
 #include <stdio.h>
@@ -108,7 +105,6 @@ void showCities(){
         int currentCity=0;
         struct station* prev = NULL;
         struct station* curr=NULL;
-        //int currentStation=0;
         while (fgets(buffer, 1024, fp)) {
             column = 0;
             row++;
@@ -177,28 +173,6 @@ void showCities(){
     }
 }
 
-int totalMaxNeighborsPerStation(struct station st){
-    int result=0;
-    for(int k=0;k<SIZE;k++)
-        if(strcmp(st.Lines[k].busLine,"")!=0)
-            result++;
-    return 2*result;
-}
-
-
-void MaxNeighbors(){
-    int max=0,temp=0;
-    char station[100];
-    for(int i=0;i<AMOUNTOFCITIES;i++)
-        for(int j=0;j<citiesList[i].amountOfStations;j++){
-            temp=totalMaxNeighborsPerStation(citiesList[i].stations[j]);
-            if(max<temp){
-            max=temp;
-            strcpy(station,citiesList[i].cityName);
-            }
-        }
-    printf("max possible neighbors: %d\t city: %s\n",max,station);
-}
 void findMaxNeghbors(){
     int counter=0,max=0;
     char station[60];
@@ -228,9 +202,8 @@ void busLineList(){
              printf("\n\tstation %s lines: \n",citiesList[i].stations[j].stationName);
             for(int k=0;k<SIZE;k++){
                 if (strcmp(citiesList[i].stations[j].Lines[k].busLine,"")!=0)
-                    printf("\t\tbus lines %d:  %s\n",k+1,citiesList[i].stations[j].Lines[k].busLine);
+                    printf("\t\tbus line %d:  %s\n",k+1,citiesList[i].stations[j].Lines[k].busLine);
             }
-            //printf("\t\tmax neighbors: %d\n",totalMaxNeighborsPerStation(citiesList[i].stations[j]));
         }
     }
 }
@@ -245,7 +218,7 @@ void printCity(){
 
 void printNeighbors(struct station* arr[]){
     for(int i=0;i<MAX_NEIGHBORS;i++){
-        if(arr[i]!=NULL/*&&strcmp(arr[i]->stationName,"")!=0*/)
+        if(arr[i]!=NULL)
             printf("\tNEIGHBOR %d:  %s\n",i+1,arr[i]->stationName);
     }
 }
@@ -256,13 +229,47 @@ void busStaionList(){
          printf("\n%s stations: \n",citiesList[i].cityName);
          for(int j=0;j<citiesList[i].amountOfStations;j++){
              printf("\tstation %d:  %s\n",j+1,citiesList[i].stations[j].stationName);
-             printNeighbors(citiesList[i].stations[j].neighbors);
-             printf("is visited: %d\n",citiesList[i].stations[j].visited);
+             //printNeighbors(citiesList[i].stations[j].neighbors);
+             //printf("is visited: %d\n",citiesList[i].stations[j].visited);
              printf("\n");
          }
          //printf("\n");
      }
 }
+void printStationByCity(char city[]){
+for(int i=0;i<AMOUNTOFCITIES;i++){
+    if(strcmp(citiesList[i].cityName,city)==0){
+    printf("\n%s stations: \n",citiesList[i].cityName);
+         for(int j=0;j<citiesList[i].amountOfStations;j++){
+         printf("\tstation %d:  %s\n",j+1,citiesList[i].stations[j].stationName);
+         }
+   }        
+}
+}
+void printBusLinesByStation(char station[]){
+    for(int i=0;i<AMOUNTOFCITIES;i++){
+         for(int j=0;j<citiesList[i].amountOfStations;j++){
+             if(strcmp(citiesList[i].stations[j].stationName,station)==0)
+             for(int k=0;k<SIZE;k++){
+               if (strcmp(citiesList[i].stations[j].Lines[k].busLine,"")!=0)
+                    printf("\t\tbus lines %d:  %s\n",k+1,citiesList[i].stations[j].Lines[k].busLine);
+            }     
+        }
+    }
+}
+
+
+
+
+void printStationNeighbors(char input[]){
+     for(int i=0;i<AMOUNTOFCITIES;i++){
+         for(int j=0;j<citiesList[i].amountOfStations;j++){
+             if(strcmp(citiesList[i].stations[j].stationName,input)==0)
+                printNeighbors(citiesList[i].stations[j].neighbors);
+         }
+    }
+}
+
 
 int totalStations(){
     int result=0;
@@ -271,56 +278,65 @@ int totalStations(){
     return result;
 }
 
-int recDis(struct station* neighbor, char endStation[], int* counter){
+int recDis(struct station* neighbor, char endStation[], int counter){
     if(neighbor==NULL)
-        return -1;
+        return 0;
     if(neighbor->visited==1)
-        return -1;
+        return 0;
     neighbor->visited=1;
-    (*counter)++;
+    if(strcmp(neighbor->stationName,endStation)==0)
+        return counter+1;
+    //(*counter)++;
+    int result=0;
+    //int max=0;
     for(int i=0;i<MAX_NEIGHBORS;i++){
-        if(neighbor==NULL)
-            return -1;
-        if(strcmp(neighbor->stationName,endStation)==0)
-            return *counter;
-        recDis(neighbor->neighbors[i], endStation,counter);
+        if(neighbor->neighbors[counter]==NULL)
+            return 0;
+        //if(strcmp(neighbor->stationName,endStation)==0)
+            //return (*counter)++;
+        //counter++;
+        result+=recDis(neighbor->neighbors[i], endStation,counter+1);
+        if(result==1)
+            return result+1;
     }
-    return *counter;
+    return result;
 }
-
 void twoStationsDis(char beginningStation[],char endStation[]){
     if(strcmp(beginningStation,"")==0||strcmp(endStation,"")==0){
         printf("Invalid input.\n");
-        return; }
+        return; 
+        }
     for(int i=0;i<AMOUNTOFCITIES;i++)
         for(int j=0;j<citiesList[i].amountOfStations;j++){
             if(strcmp(citiesList[i].stations[j].stationName,beginningStation)==0){
                 if(strcmp(beginningStation,endStation)==0){
                     printf("Same station.\n");
-                    return; }
+                    return; 
+                    }
                 if(citiesList[i].stations[j].neighbors[0]==NULL){
                     printf("Path not found.\n");
-                    return; }
+                    return; 
+                    }
                 int curr=0;
-                int result=-1,path=0;
+                int result=0,path=0;
                 int counter=0;
-                int* counterptr=&counter;
                 struct station* current=citiesList[i].stations[j].neighbors[curr];
                 while (current!=NULL) {
-                    path=recDis(current,endStation,counterptr);
-                    if(path!=-1){
-                        if(result==-1)
+                    path=recDis(current,endStation,counter);
+                    if(path!=0){
+                        if(result==0)
                             result=path;
                         else
                             result=result>path? path:result;
                     }
+                    initVisited();
                     current=citiesList[i].stations[j].neighbors[curr++];
                     }
-                if(result==-1){
+                if(result==0){
                     printf("Path not found.\n");
                     return; }
                 else
-                    printf("There is %d stations between them.\n",result);
+                    printf("\"%s\" is %d stations away from \"%s\".\n",endStation,result,beginningStation);
                     return;
                 }
             }
@@ -328,46 +344,28 @@ void twoStationsDis(char beginningStation[],char endStation[]){
             return;
         }
 
-void twoStations(char beginningStation[],char endStation[]){
-    for(int i=0;i<AMOUNTOFCITIES;i++)
-        for(int j=0;j<citiesList[i].amountOfStations;j++){
-            if(strcmp(citiesList[i].stations[j].stationName,beginningStation)==0){
-                if(strcmp(beginningStation,endStation)==0){
-                    printf("Same station.\n");
-                    return;
-                    }
-                int counter=-1;
-                for(int k=0; k<MAX_NEIGHBORS;k++){
-                    while(citiesList[i].stations[j].neighbors[k]!=NULL){
-                        for(int x=0; x<MAX_NEIGHBORS;x++){
-                        while(citiesList[i].stations[j].neighbors[k]->neighbors[x]!=NULL){
-                            if(strcmp(citiesList[i].stations[j].neighbors[k]->neighbors[x]->stationName,endStation)){
-                                //counter++;
-                                break;
-                                }
-                            counter++;
-                        }
-                    }
-                }
-            }
-}
-        }
-}
+
 
 
 int main(){
     initAmountOfStations();
     initVisited();
     showCities();
-    char beg[60],end[60];
+    char beg[60],end[60],city[20],station[60];
     for(;;){
-        //printf("\033[1;34m");
-        printf("\n\nPlease choose option:\n");
-        printf("Press 1 to see the cities list\n");
-        printf("Press 2 to see the staions list\n");
-        printf("Press 3 to see which bus is passing at a particular station\n");
-        printf("Press 4 to see the distance from one city to another with reference to bus stops\n");
-        printf("Press 5 to see the distance from one station to another\n");
+        printf("\033[1;36m");
+      
+        printf("\n~~~~~SuperBus company~~~~~\n");
+         printf("\033[1;37m");
+        printf("\nPlease choose option:\n");
+        printf("1: Show all the cities list\n");
+        printf("2: Show all stations for each city\n");
+        printf("3: Show all the  bus lines that passing at a particular station\n");
+        printf("4: Show stations for specific city\n");
+        printf("5: Show bus lines for specific station\n");
+        printf("6: Show the distance from one city to another with reference to bus stops\n");
+        printf("7: Show the distance from one station to another\n");
+        printf("8: Show a specific station's neighbors\n ");
         printf("Press 9 to exit\n");
 
         int option;
@@ -376,9 +374,6 @@ int main(){
         {
             case 1:
                 printCity();
-                //printf("\n\n total stations: %d",totalStations());
-                //MaxNeighbors();
-                findMaxNeghbors();
                 break;
             case 2:
                 busStaionList();
@@ -387,18 +382,39 @@ int main(){
                 busLineList();
                 break;
             case 4:
-                //disCity();//show the dis between one city and other
+             printf("Enter city name\n");
+                getchar();
+                fgets(city, 60, stdin);
+                city[strlen(city) - 1] = 0;
+                 printStationByCity(city);
                 break;
             case 5:
-                printf("enter boarding station\n");
+                 printf("Enter station name\n");
+                getchar();
+                fgets(station, 60, stdin);
+                station[strlen(station) - 1] = 0;
+                printBusLinesByStation(station);
+                break;
+            case 6:
+                //disCity();//show the dis between one city and other
+                break;
+            case 7:
+                printf("Enter boarding station\n");
                 getchar();
                 fgets(beg, 60, stdin);
                 beg[strlen(beg) - 1] = 0;
-                printf("enter drop-off station\n");
+                printf("\nenter drop-off station\n");
                 fgets(end, 60, stdin);
                 end[strlen(end) - 1] = 0;
                 getchar();
                 twoStationsDis(beg,end);
+                break;
+            case 8:
+                printf("enter station\n");
+                getchar();
+                fgets(beg, 60, stdin);
+                beg[strlen(beg) - 1] = 0;
+                printStationNeighbors(beg);
                 break;
             case 9:
                 return 0;
